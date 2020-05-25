52f50a9a6c6a2afca5f144152ab0a24fc15707ff
jemunm378 <64645131+jemunm378@users.noreply.github.com>
2020-05-25 13:53:19 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   session_start(); /* Starts the session */
 
   if($_SESSION['Active'] == false){ /* Redirects user to Login.php if not logged in */
-    header("location:login.php");
+    header("location:login.html");
 	  exit;
   }
 ?>
